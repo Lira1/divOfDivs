a2c56ee0e295348ec84fdda2fc77748ef96e8db1
WLira <liracalero1996@gmail.com>
2024-03-08 20:07:30 -0600

    Agregando las intruciones para la tarea

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+const divPrincipal = document.querySelector('.principal')
+//divPrincipal.textContent = 'Hola';
+/*
+    1. Crear divs hijos de divPrincipal, en total 16*16
+    2. Usando flexbox, en una linea quepan 16divs
+    3. Darle estilos con css
+    4. Cambiar el background-color del div cuando se pase sobre el div
+        4.1 Que los colores sean aleatorios
+    5. Agregar un boton para que el usuario pueda cambiar la cantidad de divs (max 64)
+ */
+
