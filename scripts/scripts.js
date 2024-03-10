7eab5d356a6cd95ed4f0d11f8e6bbca56bb8036e
WLira <liracalero1996@gmail.com>
2024-03-10 17:20:30 -0600

    Creando el block de dibujos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,41 @@
-const divPrincipal = document.querySelector('.principal')
-//divPrincipal.textContent = 'Hola';
-/*
-    1. Crear divs hijos de divPrincipal, en total 16*16
-    2. Usando flexbox, en una linea quepan 16divs
-    3. Darle estilos con css
-    4. Cambiar el background-color del div cuando se pase sobre el div
-        4.1 Que los colores sean aleatorios
-    5. Agregar un boton para que el usuario pueda cambiar la cantidad de divs (max 64)
- */
+const divPrincipal = document.querySelector('.principal');
+const nuevoBlock = document.querySelector('#clear');
 
+for (let i = 0; i < 136 ; i++){
+    const divHorizontal = document.createElement('div');
+    divHorizontal.classList.add('horizontales');
+    divHorizontal.id = "linea"+i;
+    for( let j = 0; j<136;j++){
+        const divVertical = document.createElement('div');
+        divVertical.classList.add('cuadros');
+        divVertical.id = "cuadro"+i+j;
+        divHorizontal.appendChild(divVertical);
+    }
+    divPrincipal.appendChild(divHorizontal);
+}
+
+function GenerarColorAleatorioDecimal(){
+    let colorAleatorioDecimal = Math.floor(Math.random()*16777216);
+    let colorAleatorioHexaDecimal = ConvertirColor2HexaDecimal(colorAleatorioDecimal);
+    return colorAleatorioHexaDecimal;
+}
+
+function ConvertirColor2HexaDecimal(colorAleatorio){
+    const colorHexadecimal = colorAleatorio.toString(16);
+    return `#${colorHexadecimal.padStart(6, "0")}`;
+}
+
+function detectarMouseMove(div) {
+    div.addEventListener("mousemove", function(event) {
+    div.style.backgroundColor = GenerarColorAleatorioDecimal();
+    });
+  }
+
+const divsCuadros = document.querySelectorAll(".cuadros");
+for (const div of divsCuadros) {
+  detectarMouseMove(div);
+}
+
+nuevoBlock.addEventListener('click', function(event){
+    nuevoBlock = location.reload();
+})
